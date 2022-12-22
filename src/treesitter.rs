ba4a98af56bca7d6c18b63d1850aaa20e24c2d84
Joseph Ferano <joseph@ferano.io>
2022-12-22 09:59:37 +0700

    Delete submodule

--- src/treesitter.rs
+++ src/treesitter.rs
@@ -2,12 +2,5 @@ use tree_sitter::{Parser, Language};
 use std::path::PathBuf;
 
 pub fn compile_md_grammar() {
-    let dir: PathBuf = ["tree-sitter-javascript", "src"].iter().collect();
-
-    cc::Build::new()
-        .include(&dir)
-        .file(dir.join("parser.c"))
-        .file(dir.join("scanner.c"))
-        .compile("tree-sitter-javascript")
 }
 

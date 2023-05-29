c9a69c7abee421ea31afd7118aaccc97ce19c98b
Joseph Ferano <joseph@ferano.io>
2023-05-29 17:02:24 +0700

    Get rid of treesitter dep, add more to readme

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,7 @@
 mod app;
 mod ui;
 mod input;
-mod treesitter;
 
 pub use app::*;
 pub use ui::draw;
 pub use input::handle_input;
-pub use treesitter::compile_md_grammar;
--- src/treesitter.rs
+++
@@ -1,6 +0,0 @@
-use tree_sitter::{Parser, Language};
-use std::path::PathBuf;
-
-pub fn compile_md_grammar() {
-}
-
